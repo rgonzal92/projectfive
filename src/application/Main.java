03bf47385e80ad50969c82676f071b0e257136f0
collinb585 <75229777+collinb585@users.noreply.github.com>
2020-12-02 11:28:19 -0600

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,8 +6,27 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Main is a Java Class that contains a main and an overwritten start method from the 
+ * extended Application Class to run the program when completed.
+ * 
+ * @author Richard Gonzalez
+ * @author Shejan Shuza
+ * @author Juan-Carlos Rodriguez
+ * @author Collin Behunin
+ * @author Jacob De Hoyos
+ * UTSA CS 3443 - Team Project: YoutubeDL
+ * Fall 2020
+ */
 public class Main extends Application {
 
+    /**
+	 * Creates the Stage based of the Main.fxml file's specifications.
+	 * This will serve as the main entry point of the JavaFX application.
+	 * 
+	 * @param	primaryStage	The primary Stage of the project.
+	 * @throws	Exception		Will be thrown if an any Exception is encountered.
+	 */
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("view/main.fxml"));
@@ -17,7 +36,11 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
+    /**
+     * Launches the application.
+     * 
+     * @param args	A String array of parameter arguments.
+     */
     public static void main(String[] args) {
         launch(args);
     }
